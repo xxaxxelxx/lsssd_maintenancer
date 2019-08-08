5bae0c981ab3ce3256a7857deede1becc4515974
xxaxxelxx <x@axxel.net>
2019-08-08 14:49:11 +0200

    Updated 2019-08-08 14:49

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -ex
 
 echo "$1" | grep -i '^exit' > /dev/null && exit 0
 
+chmod a+rwx /volumes/CommandVolume
+chmod a+rw /volumes/CommandVolume/WATCHLIST
+
 ./maintain_db.sh $MYSQL_ROOT_PASSWORD $MYSQL_HOST $MYSQL_PORT /volumes/CommandVolume/WATCHLIST /volumes/CommandVolume/CONFIG_MASTER
 
 exit $?
